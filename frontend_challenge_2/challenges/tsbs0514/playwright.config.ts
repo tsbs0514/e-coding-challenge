1f29cb157e8507b3c8c36717b2b6c68efa985515
tsbs0514 <tsbs.ng.0514@gmail.com>
2025-08-08 00:14:25 +0900

    playwrightのci最適化

--- frontend_challenge_2/challenges/tsbs0514/playwright.config.ts
+++ frontend_challenge_2/challenges/tsbs0514/playwright.config.ts
@@ -6,25 +6,33 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: process.env.CI ? "dot" : "html",
   use: {
     baseURL: "http://127.0.0.1:3000",
     trace: "on-first-retry",
+    headless: true,
   },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
-  ],
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+        {
+          name: "Mobile Chrome",
+          use: { ...devices["Pixel 5"] },
+        },
+        {
+          name: "Mobile Safari",
+          use: { ...devices["iPhone 12"] },
+        },
+      ],
   webServer: {
     command: "npm run dev",
     url: "http://127.0.0.1:3000",
