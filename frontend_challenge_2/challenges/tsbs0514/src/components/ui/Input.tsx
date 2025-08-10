26a7b4a10e1ab48f0c7421a66662845c8cb3d0b8
tsbs0514 <tsbs.ng.0514@gmail.com>
2025-08-10 23:31:39 +0900

    UIのpaddingを統一

--- frontend_challenge_2/challenges/tsbs0514/src/components/ui/Input.tsx
+++ frontend_challenge_2/challenges/tsbs0514/src/components/ui/Input.tsx
@@ -12,7 +12,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "flex w-full bg-white px-4 py-1 text-base placeholder:text-gray-300 focus:border-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500/20 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex w-full bg-white px-4 py-2 text-base placeholder:text-gray-300 focus:border-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500/20 disabled:cursor-not-allowed disabled:opacity-50",
             error && "border-red-500 focus:border-red-500 focus:ring-red-500/20"
           )}
           ref={ref}
